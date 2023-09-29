2f77a94d0783a9efb397f74b3f2dee0bde5cf978
Nazanin Hashemian <na.hashemian@gmail.com>
2023-09-29 13:56:56 -0700

    key update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "d4f918c7dc4542cd91ca80da0bc0ab96",
   },
 });
