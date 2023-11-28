757208c1a1a97fcec5f64035cb43a9d3f29a376c
Nazanin Hashemian <na.hashemian@gmail.com>
2023-11-28 01:07:41 -0800

    Build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,8 @@
 import { Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
-import DefinitionItem from '../components/DefinitionItem';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
-import genres from '../data/genres';
+import GameTrailer from '../components/GameTrailer';
 import useGame from '../hooks/useGame';
 import CriticScore from './../components/CriticScore';
 
@@ -12,14 +11,14 @@ const GameDetailPage = () => {
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
-
+  
   if (error || !game) throw error;
-
   return (
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game}></GameAttributes>
+      <GameTrailer gameId={game.id} />
     </>
   )
 }
